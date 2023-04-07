7b9fce5f08c2a72ade3807909096ac6498a8ea6d
Oscar Daniel Acho <oscaracho132@gmail.com>
2023-04-07 15:07:50 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/NavBar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Developers from './components/Developers'
+import Subscribe from './components/Subscribe'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Developers/>
+      <Subscribe/>
+    </div>
+  )
+}
+
+export default App
